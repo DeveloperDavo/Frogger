2857ccd4c6af619d2c32fa71bb0d88f9d95d9985
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-09-30 16:42:45 +0200

    npm test now watches for changes

--- gulpfile.js
+++ gulpfile.js
@@ -10,3 +10,14 @@ gulp.task('test', function (done) {
         singleRun: true
     }, done).start();
 });
+
+/**
+ * Watch for file changes and re-run tests on each change
+ */
+gulp.task('tdd', function (done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js'
+    }, done).start();
+});
+
+gulp.task('default', ['tdd']);
