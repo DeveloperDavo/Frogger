c93fcaa6f6b0307c2636a3df4c468b4216f54e89
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-10-01 14:15:21 +0200

    Update karma.conf.js to only test app.js

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'js/*.js',
+            'js/app.js',
             'spec/*[sS]pec.js'
         ],
 
