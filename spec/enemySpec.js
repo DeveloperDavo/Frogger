7b3672a63c3b6ff26718cfede9538676bc87fc24
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-10-01 16:56:51 +0200

    Fix typo in enemySpec

--- spec/enemySpec.js
+++ spec/enemySpec.js
@@ -1,6 +1,6 @@
 describe("Enemy", function() {
     it("update should increase position x by speed * dt", function() {
-        var x = 0, y = 50, speed = 200; dt = 0.01;
+        var x = 0, y = 50, speed = 200, dt = 0.01;
         var enemy = new Enemy(x, y, speed);
 
         enemy.update(dt);
