9ca4e70a2cc2196b02bf5b2a57455c47e3b73021
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-10-03 08:13:32 +0200

    Update karma conf to include all js files in js directory

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'js/app.js', 'js/enemy.js', 'js/player.js',
+            'js/*.js',
             'spec/*[sS]pec.js'
         ],
 
