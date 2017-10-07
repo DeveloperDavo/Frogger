8dc6fa0724738155af81c6b73d496aa8c1a5a996
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-10-07 10:02:10 +0200

    Extract getUpdateX and getUpdateY

--- js/player.js
+++ js/player.js
@@ -2,23 +2,28 @@ var Player = function (position) {
     this.position = position;
     this.sprite = 'images/char-boy.png';
 
-    this.update = function () {
-        var x = this.position.x;
+    var getUpdatedY = function () {
         var y = this.position.y;
-
-        if (x >= X_MAX) {
-            x = X_MAX;
-        } else if (x <= 0) {
-            x = 0;
-        }
-
         if (y > Y_MAX) {
             y = Y_MAX;
         } else if (y <= Y_WIN) {
             y = Y_START;
         }
+        return y;
+    };
 
-        this.position = new Position(x, y);
+    var getUpdatedX = function () {
+        var x = this.position.x;
+        if (x >= X_MAX) {
+            x = X_MAX;
+        } else if (x <= 0) {
+            x = 0;
+        }
+        return x;
+    };
+
+    this.update = function () {
+        this.position = new Position(getUpdatedX.call(this), getUpdatedY.call(this));
     };
 
     this.handleInput = function (direction) {
