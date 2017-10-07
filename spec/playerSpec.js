d8859d921a8a90e6b36497f7b473c1023142ef0b
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-10-07 09:05:17 +0200

    Extract x and y in playerSpec

--- spec/playerSpec.js
+++ spec/playerSpec.js
@@ -1,38 +1,42 @@
 describe("Player", function () {
 
     var player;
+    var x;
+    var y;
 
     beforeEach(function() {
-        player = new Player(new Position(200, 300));
+        x = 1 * BLOCK_WIDTH;
+        y = HEIGHT_WITHIN_BLOCK;
+        player = new Player(new Position(x, y));
     });
 
     it("should move left when pressing left", function () {
 
         player.handleInput('left');
 
-        expect(player.position.x).toBe(200 - BLOCK_WIDTH);
-        expect(player.position.y).toBe(300);
+        expect(player.position.x).toBe(x - BLOCK_WIDTH);
+        expect(player.position.y).toBe(y);
     });
 
     it("should move right when pressing right", function () {
 
         player.handleInput('right');
 
-        expect(player.position.x).toBe(200 + BLOCK_WIDTH);
-        expect(player.position.y).toBe(300);
+        expect(player.position.x).toBe(x + BLOCK_WIDTH);
+        expect(player.position.y).toBe(y);
     });
 
     it("should move up when pressing up", function () {
         player.handleInput('up');
 
-        expect(player.position.x).toBe(200);
-        expect(player.position.y).toBe(300 - BLOCK_HEIGHT);
+        expect(player.position.x).toBe(x);
+        expect(player.position.y).toBe(y - BLOCK_HEIGHT);
     });
 
     it("should move down when pressing down", function () {
         player.handleInput('down');
 
-        expect(player.position.x).toBe(200);
-        expect(player.position.y).toBe(300 + BLOCK_HEIGHT);
+        expect(player.position.x).toBe(x);
+        expect(player.position.y).toBe(y + BLOCK_HEIGHT);
     });
 });
